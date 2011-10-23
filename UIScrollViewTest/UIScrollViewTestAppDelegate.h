7e94f89d070c562ee8326af766efbb0350a6e971
Sebastien Metrot <meeloo@meeloo.net>
2011-10-24 01:34:29 +0200

    Initial Commit

---
+++ UIScrollViewTest/UIScrollViewTestAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UIScrollViewTestAppDelegate.h
+//  UIScrollViewTest
+//
+//  Created by Sébastien Métrot on 10/24/11.
+//  Copyright 2011 Yasound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollViewTestAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ UIScrollViewTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UIScrollViewTest
+//
+//  Created by Sébastien Métrot on 10/24/11.
+//  Copyright 2011 Yasound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, nil);
+  [pool release];
+  return retVal;
+}
