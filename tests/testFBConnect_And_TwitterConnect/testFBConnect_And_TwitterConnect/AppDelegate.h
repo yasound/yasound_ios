0b03eb51546e73f6b683c7c988feb6f2af4b8c66
Loïc Berthelot <loic.berthelot@mxp4.com>
2011-11-10 18:27:25 +0100

    added empty testFBConnect_And_TwitterConnect test program

---
+++ tests/testFBConnect_And_TwitterConnect/testFBConnect_And_TwitterConnect/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testFBConnect_And_TwitterConnect
+//
+//  Created by LOIC BERTHELOT on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ tests/testFBConnect_And_TwitterConnect/testFBConnect_And_TwitterConnect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testFBConnect_And_TwitterConnect
+//
+//  Created by LOIC BERTHELOT on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
