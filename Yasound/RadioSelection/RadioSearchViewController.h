fc687bd1fb54f976d52222d4cc48d7a203217e5a
Loïc Berthelot <loic.berthelot@mxp4.com>
2011-11-28 11:12:31 +0100

    added empty RadioSEarchViewController

---
+++ Yasound/RadioSelection/RadioSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  RadioSearchViewController.h
+//  Yasound
+//
+//  Created by LOIC BERTHELOT on 28/11/11.
+//  Copyright (c) 2011 Yasound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadioSearchViewController : UIViewController
+
+@end
