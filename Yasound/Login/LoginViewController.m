4a15b3443c9342f44d16d542028f2de428237e90
Loïc Berthelot <neywen@neywen.net>
2011-12-08 19:16:56 +0100

    cleaning

--- Yasound/Login/LoginViewController.m
+++ Yasound/Login/LoginViewController.m
@@ -84,8 +84,8 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
     if (cell == nil) 
-    {
-        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
+    {   
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
     }
     
     switch (indexPath.row)
