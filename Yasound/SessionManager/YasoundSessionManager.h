e39a3fc20909f22da6ea7b870a1f3d7301675e4f
Loïc Berthelot <neywen@neywen.net>
2012-01-05 01:12:06 +0100

    added missing files

---
+++ Yasound/SessionManager/YasoundSessionManager.h
@@ -0,0 +1,42 @@
+//
+//  YasoundSessionManager.h
+//  Yasound
+//
+//  Created by LOIC BERTHELOT on 04/01/12.
+//  Copyright (c) 2012 Yasound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define LOGIN_TYPE_YASOUND @"yasound"
+#define LOGIN_TYPE_FACEBOOK @"facebook"
+#define LOGIN_TYPE_TWITTER @"twitter"
+
+
+@interface YasoundSessionManager : NSObject
+{
+    NSMutableDictionary* _dico;
+    id _target;
+    SEL _action;
+}
+
+@property (nonatomic) BOOL registered;
+@property (nonatomic, retain) NSString* loginType;
+
++ (YasoundSessionManager*)main;
+
+- (void)save;
+
+// return NO if no login information have been regitered yes => launch login dialog in this case
+// return YES if login information have been registered => use them to log in automatically, and callback the given action when login process ends.
+- (BOOL)loginForYasoundWithTarget:(id)target action:(SEL)action;
+- (BOOL)loginForFacebookWithTarget:(id)target action:(SEL)action;
+- (BOOL)loginForTwitterWithTarget:(id)target action:(SEL)action;
+- (void)logoutWithTarget:(id)target action:(SEL)action;
+
+// register login information
+- (void)registerForYasound:(NSString*)email withPword:(NSString*)pword;
+- (void)registerForFacebook; // login info are handle by SocialSessionManager
+- (void)registerForTwitter; // login info are handle by SocialSessionManager
+
+@end
