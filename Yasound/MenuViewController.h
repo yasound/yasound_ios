3e44502347da5ec9ae0c0f9bfa0d3ea78a5512c5
Loïc Berthelot <neywen@neywen.net>
2012-01-25 10:54:54 +0100

    added DDMenuController

---
+++ Yasound/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Yasound
+//
+//  Created by LOIC BERTHELOT on 25/01/12.
+//  Copyright (c) 2012 Yasound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
