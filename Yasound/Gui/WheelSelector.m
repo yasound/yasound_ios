aada8bb51453f7a7e28bbe914b7b2c2af21c8c63
Loïc Berthelot <neywen@neywen.net>
2012-07-19 17:35:44 +0200

    cleaning

--- Yasound/Gui/WheelSelector.m
+++ Yasound/Gui/WheelSelector.m
@@ -23,10 +23,6 @@
 
 - (void)awakeFromNib
 {
-//    self = [super initWithFrame:frame];
-//    if (self) 
-//    {
-    
     self.needsToStick = NO;
     
     // compute selector's options
@@ -50,7 +46,7 @@
         CGFloat spacing = 48;
         CGFloat width = 0;
     
-        // add labels for the selector options, compute the contents size, and the sticky positions of the scrollview
+        // add labels for the selector options, and compute the sticky positions for all items
         for (NSMutableArray* item in self.items)
         {
             UILabel* label = [sheet makeLabel];
@@ -72,27 +68,21 @@
             x += spacing;
         }
 
+    // compute the contents size and set scrollview's behavior
     self.contentSize = CGSizeMake(x - width + marginRight, self.contentSize.height);
     [self setShowsHorizontalScrollIndicator:NO];
     [self setShowsVerticalScrollIndicator:NO];
     self.delegate = self;
     self.decelerationRate = UIScrollViewDecelerationRateFast;
-//    self.decelerationRate = UIScrollViewDecelerationRateNormal;
-    
-//    // add selector visual indicator
-//    sheet = [[Theme theme] stylesheetForKey:@"WheelSelector.indicator" retainStylesheet:YES overwriteStylesheet:NO error:nil];
-//    UIImageView* indicator = [sheet makeImage];
-//    indicator.frame = CGRectMake(self.frame.size.width/2.f - indicator.frame.size.width/2.f, self.frame.size.height - indicator.frame.size.height, indicator.frame.size.width, indicator.frame.size.height);
-//    [self addSubview:indicator];
 
+    // add visual layer for the selector indicator (the red needle)
     sheet = [[Theme theme] stylesheetForKey:@"WheelSelector.indicator" retainStylesheet:YES overwriteStylesheet:NO error:nil];
     UIImage* indicatorImage = [sheet image];
     CGRect indicatorFrame = CGRectMake(self.frame.size.width/2.f - indicatorImage.size.width/2.f, self.frame.origin.y + self.frame.size.height - indicatorImage.size.height, indicatorImage.size.width, indicatorImage.size.height);
     CALayer* indicatorLayer = [CALayer layer];
     indicatorLayer.contents = (id)[indicatorImage CGImage];
     indicatorLayer.frame = indicatorFrame;
-    
-    [[self.superview layer] addSublayer:indicatorLayer];
+    [[self.superview layer] addSublayer:indicatorLayer]; // to superview 'cause it must not scroll
     
 }
 
@@ -121,7 +111,6 @@
 
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
-//- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView
 {
     if (!self.needsToStick)
         return;
@@ -152,41 +141,14 @@
     
     // translate the stickyPos, for the scrollview position reference
     stickyPos -= self.frame.size.width/2.f;
-    
-    // make the scrollview goes to the sticky position
-//    [UIView beginAnimations:nil context:NULL];
-//    [UIView setAnimationDuration:0.33];
-//    self.contentOffset = CGPointMake(stickyPos, self.contentOffset.y);
-//    [UIView commitAnimations];
+
     [self setContentOffset: CGPointMake(stickyPos, self.contentOffset.y) animated:YES];
 }
 
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-//    NSLog(@"%.2f", self.contentOffset.x);
-//    
-//    NSInteger i =0;
-//    for (NSArray* item in self.items)
-//    {
-//    CGFloat stickyPos = [[item objectAtIndex:ITEM_STICKY_POS] floatValue];
-//
-//    NSLog(@"sticky [%d] : %.2f", i, stickyPos);
-//        i++;
-//    }
-
-//    if (!_waitingForPreviousEvents)
-//    {
-//        float offset = scrollView.contentOffset.y;
-//        float contentHeight = scrollView.contentSize.height;
-//        float viewHeight = scrollView.bounds.size.height;
-//        
-//        if (offset + viewHeight > contentHeight + WALL_WAITING_ROW_HEIGHT)
-//        {
-//            [self askForPreviousEvents];
-//        }
-//    }
-}
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+//{
+//}
 
 
 
@@ -196,18 +158,6 @@
 // An empty implementation adversely affects performance during animation.
 //- (void)drawRect:(CGRect)rect
 //{
-//    // add selector visual indicator
-//    BundleStylesheet* sheet = [[Theme theme] stylesheetForKey:@"WheelSelector.indicator" retainStylesheet:YES overwriteStylesheet:NO error:nil];
-////        UIImageView* indicator = [sheet makeImage];
-//    UIImage* indicator = [sheet image];
-//        CGRect frame = CGRectMake(self.frame.size.width/2.f - indicator.size.width/2.f, self.frame.size.height - indicator.size.height, indicator.size.width, indicator.size.height);
-////        
-////        
-////        
-////        [self addSubview:indicator];
-//    
-//    
-//    [indicator drawInRect:frame];
 //}
 
 
